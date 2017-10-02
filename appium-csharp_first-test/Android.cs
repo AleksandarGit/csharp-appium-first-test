d774e84ea52138f37d3e653f120846d0f2896db3
nivi.mor <nivi.mor@experitest.com>
2017-10-02 12:21:14 +0300

    first commit

---
+++ appium-csharp_first-test/Android.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.iOS;
+using NUnit;
+using NUnit.Framework;
+using System.Collections.Generic;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Service.Options;
+
+namespace appium
+{
+    [TestFixture]
+    public class Android
+    {
+        private string projectName = "reports";
+        private string accessKey = "";
+
+        protected AndroidDriver<AndroidElement> driver = null;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+
+        [SetUp()]
+        public void SetupTest()
+        {
+
+            dc.SetCapability("accessKey", accessKey);
+            dc.SetCapability("projectName", projectName);
+            //install the app on the device
+            dc.SetCapability(MobileCapabilityType.App, "cloud:<BUNDLE_ID>");
+
+            dc.SetCapability("platformName", "Android");
+            //launch the app
+            dc.SetCapability(AndroidMobileCapabilityType.AppPackage, "<BUNDLE_ID>");
+            dc.SetCapability(AndroidMobileCapabilityType.AppActivity, "<ACTIVITY>");
+            driver = new AndroidDriver<AndroidElement>(new Uri("https://cloud.experitest.com:443/wd/hub"), dc);
+
+        }
+
+        [Test()]
+        public void TestUntitled()
+        {
+
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            driver.Lock();
+            driver.Quit();
+        }
+    }
+}
---
+++ appium-csharp_first-test/iOS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.iOS;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.Enums;
+
+
+namespace appium
+{
+    [TestFixture]
+    public class iOS
+    {
+        private string projectName = "reports";
+        private string accessKey = "";
+        protected IOSDriver<IOSElement> driver = null;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+
+        [SetUp()]
+        public void SetupTest()
+        {
+            dc.SetCapability("accessKey", accessKey);
+            dc.SetCapability("projectName", projectName);
+            dc.SetCapability(MobileCapabilityType.PlatformName, "iOS");
+            dc.SetCapability(MobileCapabilityType.App, "cloud:<BUNDLE_ID>");
+            dc.SetCapability(IOSMobileCapabilityType.BundleId, "<BUNDLE_ID>");
+            driver = new IOSDriver<IOSElement>(new Uri("https://cloud.experitest.com:443/wd/hub"), dc);
+
+        }
+        [Test()]
+        public void TestUntitled()
+        {
+
+
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
